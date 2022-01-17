c5a32b2cf2c6fbd7ea04544178162864bbc8b753
wanghexing <wanghexing@ly-sky.com>
2022-01-17 10:04:00 +0800

    first

--- src/components/Scheduler/index.js
+++ src/components/Scheduler/index.js
@@ -3,8 +3,15 @@
  * @Author: wanghexing
  * @Date: 2022-01-08 10:25:09
  * @LastEditors: wanghexing
- * @LastEditTime: 2022-01-08 10:25:09
+ * @LastEditTime: 2022-01-17 10:02:47
  */
-import Scheduler from './Scheduler';
-import './Scheduler.css';
-export default Scheduler;
+// /*
+//  * @Description: 
+//  * @Author: wanghexing
+//  * @Date: 2022-01-08 10:25:09
+//  * @LastEditors: wanghexing
+//  * @LastEditTime: 2022-01-08 10:25:09
+//  */
+// import Scheduler from './Scheduler';
+// import './Scheduler.css';
+// export default Scheduler;
--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,10 +1,17 @@
 /*
  * @Description: 
  * @Author: wanghexing
- * @Date: 2022-01-08 16:35:46
+ * @Date: 2022-01-10 09:33:42
  * @LastEditors: wanghexing
- * @LastEditTime: 2022-01-10 09:06:16
+ * @LastEditTime: 2022-01-17 10:02:42
  */
-import Timeline from './Timeline';
-import './Timeline.less';
-export default Timeline;
+// /*
+//  * @Description: 
+//  * @Author: wanghexing
+//  * @Date: 2022-01-08 16:35:46
+//  * @LastEditors: wanghexing
+//  * @LastEditTime: 2022-01-10 09:06:16
+//  */
+// import Timeline from './Timeline';
+// import './Timeline.less';
+// export default Timeline;
