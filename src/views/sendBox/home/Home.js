4c5092c915b32b55cc58d8dee56a0abf6bc36cbc
Fortitude-lan <358098351@qq.com>
2022-01-18 18:02:30 +0800

    更新

--- src/views/sendBox/home/Home.js
+++ src/views/sendBox/home/Home.js
@@ -3,7 +3,7 @@
  * @Author: wanghexing
  * @Date: 2022-01-13 17:16:41
  * @LastEditors: wanghexing
- * @LastEditTime: 2022-01-14 17:50:59
+ * @LastEditTime: 2022-01-18 15:02:18
  */
 import React from 'react'
 import { Button } from 'antd';
@@ -11,28 +11,28 @@ import axios from 'axios';
 export default function Home() {
     const ajex = () => {
         //取数据
-        // axios.get("http://localhost:8000/posts").then(res => { console.log(res.data); })
+        // axios.get("http://localhost:5500/posts").then(res => { console.log(res.data); })
         //增
-        // axios.post("http://localhost:8000/posts", {
+        // axios.post("http://localhost:5500/posts", {
         //     "title": "5555",
         //     "author": "c"
         // })
         //改 替换原字段
-        // axios.put("http://localhost:8000/posts/1", {
+        // axios.put("http://localhost:5500/posts/1", {
         //     "title": "11-改",
         //     "author": "a"
         // })
         //改 补丁求情更新
-        // axios.patch("http://localhost:8000/posts/1", {
+        // axios.patch("http://localhost:5500/posts/1", {
         //     "title": "11-改",
         //     "author": "a"
         // })
         //删
-        // axios.delete("http://localhost:8000/posts/3")
+        // axios.delete("http://localhost:5500/posts/3")
         //关联表_embed 向下关联
-        // axios.get("http://localhost:8000/posts?_embed=comments").then(res => { console.log(res.data); })
+        // axios.get("http://localhost:5500/posts?_embed=comments").then(res => { console.log(res.data); })
         //关联表_expand 向上关联
-        // axios.get("http://localhost:8000/comments?_expand=post").then(res => { console.log(res.data); })
+        // axios.get("http://localhost:5500/comments?_expand=post").then(res => { console.log(res.data); })
     }
     return (
         <div>
--- src/views/sendBox/right-manage/RightList.js
+++ src/views/sendBox/right-manage/RightList.js
@@ -3,28 +3,40 @@
  * @Author: wanghexing
  * @Date: 2022-01-13 17:19:21
  * @LastEditors: wanghexing
- * @LastEditTime: 2022-01-18 11:49:08
+ * @LastEditTime: 2022-01-18 16:36:55
  */
 import React, { useEffect, useState } from 'react'
-import { Table, Tag, Button, Modal } from 'antd';
+import { Spin, Table, Tag, Button, Modal } from 'antd';
 import axios from 'axios';
 import { SearchOutlined, EditOutlined, DeleteOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 const { confirm } = Modal
 
 export default function RightList() {
     const [dataSource, setdataSource] = useState([])
+    const [loading, setloading] = useState(false)
+    const [refresh, setRefresh] = useState(false);
+
     useEffect(() => {
-        axios.get('http://localhost:8000/rights?_embed=children').then(res => {
+        axios.get('http://localhost:5500/rights?_embed=children').then(res => {
             const list = res.data;
-            //没有子节点的不需要展开
-            list.map(item => {
+            setdataSource(mapList(list))
+            setloading(false)
+        })
+    }, [refresh])
+   
+    //遍历循环权限列表，没有子节点的不需要展开
+    const mapList = (list) => {
+        list.map(item => {
+            if (item.children) {
                 if (item.children.length == 0) {
                     item.children = ''
+                } else {
+                    mapList(item.children)
                 }
-            })
-            setdataSource(list)
+            }
         })
-    }, [])
+        return list
+    }
     const columns = [
         {
             title: 'ID',
@@ -49,8 +61,8 @@ export default function RightList() {
             render: (record) => {
                 return (
                     <div>
-                        <Button type="primary" shape="circle" icon={<EditOutlined />}  />
-                        <Button danger shape="circle" icon={<DeleteOutlined />} onClick={() => deleteMethod(record)}/>
+                        <Button type="primary" shape="circle" icon={<EditOutlined />} />
+                        <Button danger shape="circle" icon={<DeleteOutlined />} onClick={() => confirmMethod(record)} />
                     </div>
                 )
             }
@@ -58,25 +70,39 @@ export default function RightList() {
         }
     ];
 
-    const deleteMethod = (record) => {
+    const confirmMethod = (record) => {
         console.log(record)
         confirm({
             title: '确定要删除吗？',
             icon: <ExclamationCircleOutlined />,
             // content: 'Bla bla ...',
-            okText: '确认',
-            cancelText: '取消',
+            onOk() { deleteMethod(record) },
+            onCancel() { }
         });
     }
+    const deleteMethod = (record) => {
+        //目前权限列表值有两层 1 rights 2 children
+        if (record.grade == 1) {
+            // setdataSource(dataSource.filter(data => data.id !== record.id));
+            axios.delete(`http://localhost:5500/rights/${record.id}`).then(setRefresh)
+
+        } else {
+            axios.delete(`http://localhost:5500/children/${record.id}`).then(setRefresh)
+        }
+
+    }
+
     return (
 
         <div>
-            <Table dataSource={dataSource} columns={columns}
-                scroll={{ y: 500 }}
-                pagination={{
-                    pageSize: 5,
-                }}
-            />
+            <Spin spinning={loading}>
+                <Table dataSource={dataSource} columns={columns}
+                    scroll={{ y: 500 }}
+                    pagination={{
+                        pageSize: 5,
+                    }}
+                />
+            </Spin>
         </div>
     )
 }
